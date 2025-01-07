b7cf84582d01ac70ba13fb54b77a5c43a0c59567
Andrew Roberts <andrew@azroberts.com>
2025-01-07 16:13:13 -0500

    Created event card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,16 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    extend: {
+      aspectRatio: {
+        'gold': '16 / 10',
+        'banner': '25 / 10'
+      },
+      height: {
+        'gold-s': '37.5%',
+        'gold-l': '62.5%'
+      }
+    }
+  }
 } satisfies Config;
